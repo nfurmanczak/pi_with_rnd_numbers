d30a470e83ef0b3bdf811a0e4636edc2d6d0d788
nfurmanc <nfurmanc@cisco.com>
2020-11-30 11:35:53 +0100

    first commit

---
+++ calc_pi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calc_pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
